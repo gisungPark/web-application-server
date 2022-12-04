e8f7ba33aaf104380f01d9d51f85f7c3414f6e73
gisung2215 <gisung2215@naver.com>
2022-12-04 15:23:09 +0900

    요구사항1. index.html 응답하기 01. HTTP 요청 정보에서 요청 URL을 추출한다. 02. 요청 URL에 해당하는 파일을 webapp 디렉토리에서 읽어 전달한다.

---
+++ src/main/java/util/HttpRequest.java
@@ -0,0 +1,30 @@
+package util;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class HttpRequest {
+
+    private String url;
+    private Map<String, String> header = new HashMap<>();
+
+
+    @Override
+    public String toString() {
+        String value = "HttpRequest{"
+                + "url='" + url + "' ";
+
+        for(String key : header.keySet()){
+            value += "" + key + "='" +header.get(key) +"' ";
+        }
+
+        return value += "}";
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,12 @@
 package webserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequest;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,19 +24,9 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-            String URI = extractedURI(in);
-            log.debug("Call URI : {}", URI);
-            byte[] body = null;
-            switch (URI) {
-                case "/index":
-                    body = Files.readAllBytes(Paths.get("./webapp" + URI + ".html"));
-                    break;
+            HttpRequest request = readReqInfo(in);
 
-                default:
-                    body = "Hello World".getBytes();
-                    break;
-            }
-
+            byte[] body = Files.readAllBytes(new File("./webapp" + request.getUrl()).toPath());
             DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
             responseBody(dos, body);
@@ -46,17 +36,38 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private String extractedURI(InputStream in) throws IOException {
+    private HttpRequest readReqInfo(InputStream in) throws IOException {
+        HttpRequest request = new HttpRequest();
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String[] info = br.readLine().split(" ");
+        String line = br.readLine();
 
-//            while(true){
-//                String line = br.readLine();
-//                if("".equals(line) || line == null) break;
-//
-//                System.out.println(line);
-//            }
-        return info[1] != null ? info[1] : " ";
+        if (line == null) {
+            request.setUrl("/index.html");
+            return request;
+        }
+
+        String[] tokens = line.split(" ");
+        request.setUrl(isBlank(tokens[1]) ? "/index.html" : tokens[1]);
+
+        while (!"".equals(line)) {
+            if (line.contains("Content-Length")) {
+                request.getHeader().put("Content-Length", line);
+                log.debug("Content-Length : {}", line);
+            }
+
+            if (line.contains("Cookie")) {
+                request.getHeader().put("Cookie", line);
+                log.debug("Cookie : {}", line);
+            }
+            line = br.readLine();
+        }
+
+        log.debug(request.toString());
+        return request;
+    }
+
+    private boolean isBlank(String url) {
+        return url == null || url.isEmpty() || "/".equals(url);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
