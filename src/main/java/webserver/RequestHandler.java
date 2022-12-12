bd8150047f7d7ffe6db490fec4be4ef0198ba2f0
gisung2215 <gisung2215@naver.com>
2022-12-13 01:39:13 +0900

    요구사항6. 사용자 목록 출력 01. 로그인 상태에서 /user/list  접근 시 사용자 목록을 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,22 +1,25 @@
 package webserver;
 
+import com.google.common.base.Strings;
 import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequest;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private final String DEFAULT_URL = "/index.html";
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -35,6 +38,7 @@ public class RequestHandler extends Thread {
 
             String[] tokens = line.split(" ");
             int contentLength = 0;
+            String cookies = "";
 
             while (!"".equals(line)) {
                 if (line.contains("Content-Length")) {
@@ -42,6 +46,7 @@ public class RequestHandler extends Thread {
                 }
                 if (line.contains("Cookie")) {
                     System.out.println(line);
+                    cookies = line;
                 }
                 line = br.readLine();
             }
@@ -64,58 +69,87 @@ public class RequestHandler extends Thread {
 
                 User findUser = DataBase.findUserById(params.get("userId"));
 
-                if(findUser != null) {
-                    if (findUser.login(params.get("password"))) {
-                        DataOutputStream dos = new DataOutputStream(out);
-                        loginSuccessHeader(dos);
+                if (findUser != null && findUser.login(params.get("password"))) {
+                        loginSuccessResource(out);
+                }
+                responseResource(out, "/user/login_failed.html");
 
-                    } else {
-                        DataOutputStream dos = new DataOutputStream(out);
-                        loginFailHeader(dos);
-                    }
-                }else{
-                    DataOutputStream dos = new DataOutputStream(out);
-                    loginFailHeader(dos);
+            } else if ("/user/list".equals(url)) {
+                if (!isLogin(cookies)) {
+                    responseResource(out, "/user/login.html");
+                    return;
                 }
 
+                Collection<User> users = DataBase.findAll();
 
-            } else {
-                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table>");
+                for (User user : users) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+
+                byte[] body = sb.toString().getBytes(StandardCharsets.UTF_8);
                 DataOutputStream dos = new DataOutputStream(out);
                 response200Header(dos, body.length);
                 responseBody(dos, body);
+
+            } else {
+                responseResource(out, "/user/login.html");
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void loginFailHeader(DataOutputStream dos) {
+    private void responseResource(OutputStream out, String url) throws IOException {
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        DataOutputStream dos = new DataOutputStream(out);
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private boolean isLogin(String cookies) {
+        Map<String, String> stringMap = HttpRequestUtils.parseCookies(cookies);
+        String logined = stringMap.get("logined");
+
+        if(Strings.isNullOrEmpty(logined) || "false".equals(logined)) return false;
+        return true;
+    }
+
+    private void loginFailResource(OutputStream out) {
         try {
+            DataOutputStream dos = new DataOutputStream(out);
             dos.writeBytes("HTTP/1.1 401 Unauthorized \r\n");
             dos.writeBytes("Content-Type: text/html \r\n");
             dos.writeBytes("Set-Cookie: logined=false \r\n");
+
+            byte[] body = Files.readAllBytes(new File("./webapp" + "/user/login_failed.html").toPath());
+            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void loginSuccessHeader(DataOutputStream dos) {
+    private void loginSuccessResource(OutputStream out) {
         try {
+            DataOutputStream dos = new DataOutputStream(out);
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html \r\n");
             dos.writeBytes("Set-Cookie: logined=true \r\n");
+            dos.writeBytes("Location: /index.html \r\n");
+            dos.writeBytes("\r\n");
+
+            byte[] body = Files.readAllBytes(new File("./webapp" + DEFAULT_URL).toPath());
+            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
-
-    private boolean isUser(User user, String password) {
-
-        if(password.equals(user.getPassword())) return true;
-        return false;
-    }
-
 
     private String getDefaultUrl(String[] tokens) {
         if(tokens.length < 1 || "/".equals(tokens[1])) return "/index.html";
